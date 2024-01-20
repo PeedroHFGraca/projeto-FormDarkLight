03528818915f09f7aaf963e8e4cc557c6bf47567
Pedro Henrique Ferreira Graça <pedro.henrrique474@gmail.com>
2024-01-20 13:37:21 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 const mode = document.querySelector('#mode_icon');
-
 mode.addEventListener('click', () => {
     const form = document.querySelector('#login_form');
 
     if(mode.classList.contains('fa-moon')){
-        
+
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
         
